50fce0d51a870c08c043beaeed24c851f3959386
Dunkacc <113391871+Dunkacc@users.noreply.github.com>
2022-09-19 19:47:46 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+students = ["Nathan", "Kelvin", "Mariama", "Barry", "Tori"
+            "Machael", "Dominic", "Jose", "Another name"]
+students.append("Pascal")
+students.insert(2, "Dan")
+courses = ["comp151", "comp150", "math130", "math120"]
+courses.append("comp250") #adds to end
+courses.insert(1, "comp143") #first value is position where you want to put in, second is the value you want to put.
+books = ["Py crash course", "Game prog", "Code complete", "Clean code", "The Pragmatic Programmer", "Py crash course"]
+books.remove("Py crash course")
+#books = ["Game prog", "Code complete", "Clean code", "The Pragmatic Programmer", "Py crash course"]
+books.pop(2)
+#books = ["Py crash course", "Game prog", "Clean code", "The Pragmatic Programmer", "Py crash course"]
